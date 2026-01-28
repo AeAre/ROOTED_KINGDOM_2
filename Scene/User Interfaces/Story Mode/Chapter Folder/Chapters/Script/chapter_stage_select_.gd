0943e98abfd2788b2e0f142fdfdfbb5a70939c1c
Postiko <andypogi345@gmail.com>
2026-01-28 18:16:55 +0800

    - Added Background 1 - Fixed some UI discrepancies

---
+++ Scene/User Interfaces/Story Mode/Chapter Folder/Chapters/Script/chapter_stage_select_.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var chp_number: int
+@export var chp_title: String
+
+@onready var stage_confirm_for_select = $"Stage Confimation"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$"UI Manager/Chapter Number".text = "Chapter " + str(chp_number)
+	$"UI Manager/Chapter Title".text = chp_title
+	stage_confirm_for_select.visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/User Interfaces/Story Mode/Chapter Folder/Chapter UIs/chapter_selection.tscn")
