41b93addabb5b90494d8671209ddc1f7732f598a
Postiko <andypogi345@gmail.com>
2026-02-01 09:46:03 +0800

    Added Settings and Fixed Main Menu Visuals
    - (unfinished and expect it to not work properly)
    - Added Volume Option
    - Added Fullscreen Option
    - Added Resolution Option

--- Scene/User Interfaces/UI scenes/Settings.gd
+++ Scene/User Interfaces/UI scenes/Settings.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var cheat_input = $VBoxContainer/CheatInput
 @onready var redeem_btn = $VBoxContainer/RedeemButton
 @onready var feedback_lbl = $VBoxContainer/FeedbackLabel
+
+var window_id = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	redeem_btn.pressed.connect(_on_redeem_pressed)
@@ -14,3 +16,33 @@ func _on_redeem_pressed():
 
 	feedback_lbl.text = result
 	cheat_input.text = ""
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+	AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+
+func _on_fullscreen_check_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	var new_size: Vector2i
+	
+	match index:
+		0:
+			new_size = Vector2i(1920, 1080)
+		1:
+			new_size = Vector2i(1600, 900)
+		2:
+			new_size = Vector2i(1280, 720)
+	
+	# Actually resize the window
+	DisplayServer.window_set_size(new_size, window_id)
+
+
+
+			
