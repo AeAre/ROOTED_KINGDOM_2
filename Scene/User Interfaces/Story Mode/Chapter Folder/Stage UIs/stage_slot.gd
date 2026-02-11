b0dcb5ca498e93468f95b9e1a5431a8fca040026
Postiko <andypogi345@gmail.com>
2026-02-12 04:14:59 +0800

    Fixed the game 1.0

--- Scene/User Interfaces/Story Mode/Chapter Folder/Stage UIs/stage_slot.gd
+++ Scene/User Interfaces/Story Mode/Chapter Folder/Stage UIs/stage_slot.gd
@@ -5,17 +5,14 @@ extends Control
 @export var Stage_img: Texture
 @export_enum("Story", "Battle") var Scene_type: String
 @export var Objective: String
-@export var availablity: bool
+@export var availability: bool
 
-@onready var availablity_label = $Interface/Availability
 @onready var stage_confirm = $"../../../../Stage Confimation"
 @onready var stage_image = $"Interface/Stage Image"
 @onready var stage_frame = $Interface/Frame
-@onready var stage_button =  $"Interface/Stage Button"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"Interface/Stage Button".text = str(Global._CURRENTLY_PLAYING_CHAPTER) + " - " + str(Stage_number)
 	stage_image.img.texture = Stage_img
 	if Scene_type == "Story":
 		stage_frame.texture = preload("res://Asset/User Interface/stage_slot_frame_story.png")
@@ -24,12 +21,12 @@ func _ready() -> void:
 	else:
 		print("Scene_type is null or the texture of the Frame is absent.")
 	
-	if availablity:
-		availablity_label.visible = false
-		stage_button.disabled = false
+	if availability:
+		$"Interface/Stage Button".disabled = false
+		$Interface/Label.visible = false
 	else:
-		availablity_label.visible = true
-		stage_button.disabled = true
+		$Interface/Label.visible = true
+		$"Interface/Stage Button".disabled = true
 		
 	# $"Stage Slot bg".texture = Stage_img
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,11 +40,19 @@ func _on_stage_button_pressed() -> void:
 	
 	stage_confirm.image.texture = Stage_img
 	if Stage != null:
-		stage_confirm.stage_scene = Stage
 		if Scene_type == "Story":
 			stage_confirm.stage_scene = Stage
 	elif Scene_type == "Battle":
 		stage_confirm.stage_scene = preload("res://Scene/User Interfaces/CharacterScenes/CharacterSelection.tscn")
 	else:
 		stage_confirm.stage_scene = null	
+	
+	if Objective != null:
+		if Scene_type == "Story":
+			stage_confirm.objective.visible = false
+		elif Scene_type == "Battle":
+			stage_confirm.objective.visible = true
+			stage_confirm.objective.text = Objective
+	else:
+		stage_confirm.objective.visible = false
 			
