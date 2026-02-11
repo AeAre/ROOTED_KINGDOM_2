1f2535d974e9ae0c65a2086412f0df0bd9368c25
winrey123 <delosreyeswinrey@gmail.com>
2026-02-12 02:34:18 +0800

    animation tweaks v2

--- Scene/User Interfaces/Story Mode/Chapter Folder/Chapter UIs/chapter_manager.gd
+++ Scene/User Interfaces/Story Mode/Chapter Folder/Chapter UIs/chapter_manager.gd
@@ -1,6 +1,4 @@
 extends Control
-
-@onready var chapter_list = $"."
 # Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
 #	pass # Replace with function body.
@@ -9,11 +7,8 @@ extends Control
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
-func chapter_selector(chapter_position: int): 
-	get_tree().change_scene_to_packed(chapter_list.get_child(chapter_position).chp_scene)
-	Global.declare_chapter(chapter_list.get_child(chapter_position).chp_number)
-	Global.from_story_mode = true
-	
+
 
 func _on_chapter_panel_pressed() -> void:
-	chapter_selector(0) # Chapter 1
+	get_tree().change_scene_to_packed($"Chapter 1".chp_scene)
+	StoryMode.declare_chapter($"Chapter 1".chp_number)
